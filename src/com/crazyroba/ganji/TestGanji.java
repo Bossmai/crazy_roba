0aceef8cbbe1b3afa01777b3b56949453d2dd99a
Alfred Shi <memoriedsea@gmail.com>
2015-02-24 13:39:47 +0800

    add some logic for ganji apk

--- src/com/crazyroba/ganji/TestGanji.java
+++ src/com/crazyroba/ganji/TestGanji.java
@@ -30,31 +30,53 @@ public class TestGanji extends RobaActivityInstrumentationTestCase2 {
 	
 	public void testSample() {
 		//com.ganji.android/.control.CityActivity com.ganji.android:id/qunzu_feature_start_btn
-		firstStart();
+		//firstStart("6.1.1");
+		int step = 100;
+		while (step > 0) {
+			randomClick();
+			step--;
+		}
 		
 	}
 	
-	private void firstStart() {
-		Log.d(TAG, "First start of Ganji");
-		
-		solo.waitForActivity("NewFeatureActivity", 10000);
-		
-		Log.d(TAG, "New feature activity displayed.");
-		
-		while(true) {
-			Log.d(TAG, "Drag up");
-			robaDrag(DragDirection.Top);
-			if (solo.waitForText("2015.01 赶集群组正式发布", 1, 2000)) {
-				robaClickOnView("com.ganji.android:id/qunzu_feature_start_btn");
-				break;
+	private void firstStart(String version) {
+		if (version.equals("6.1.1")) {
+			Log.d(TAG, "First start of Ganji");
+			
+			solo.waitForActivity("NewFeatureActivity", 10000);
+			
+			Log.d(TAG, "New feature activity displayed.");
+			
+			while(true) {
+				Log.d(TAG, "Drag up");
+				robaDrag(DragDirection.Top);
+				if (solo.waitForText("2015.01 赶集群组正式发布", 1, 2000)) {
+					robaClickOnView("com.ganji.android:id/qunzu_feature_start_btn");
+					break;
+				}
 			}
+			
+			solo.clickOnText("上海", 1, true);
+			
+			Log.d(TAG, "Go into City Activity");
+			
+			Log.d(TAG, "Finish first start activies");
+			return;
 		}
 		
-		solo.clickOnText("上海", 1, true);
+	}
+	
+	private void randomClick() {
+		Log.d(TAG, "Click on random view.");
+		try {
+			robaRandomClickOnView();
+		} catch (Exception e) {
+			;
+		}
 		
-		Log.d(TAG, "Go into City Activity");
 		
-		Log.d(TAG, "Finish first start activies");
+		Log.d(TAG, "Sleep for 10 seconds.");
+		solo.sleep(10000);
 	}
 	
 	@Override
