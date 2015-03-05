c4020b6270bd774b9e00bb94bfda298eac618b41
Alfred Shi <memoriedsea@gmail.com>
2015-03-05 11:50:51 +0800

    uupdate kuwo tests

--- src/com/crazyroba/kuwo/TestKuWo.java
+++ src/com/crazyroba/kuwo/TestKuWo.java
@@ -1,5 +1,6 @@
 package com.crazyroba.kuwo;
 
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -7,7 +8,9 @@ import com.crazyroba.RobaActivityInstrumentationTestCase2;
 import com.jayway.android.robotium.solo.Solo;
 
 public class TestKuWo extends RobaActivityInstrumentationTestCase2 {
-	private static final boolean firstStart = false;
+	private static final String APK_VERSION = "6.6.6.0";
+
+	private static boolean isFirstStart = false;
 	
 	//private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "cn.kuwo.player/cn.kuwo.ui.guide.GuideActivity";
 	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "cn.kuwo.player.activities.MainActivity";
@@ -33,34 +36,58 @@ public class TestKuWo extends RobaActivityInstrumentationTestCase2 {
 	}
 	
 	public void test1() {
-		if (firstStart) {
-			firstStart("6.6.6.0");
-		}
+		CheckFirstStart(APK_VERSION);
 		
+		Log.d(TAG, "Play random music.");
+		
+		playMusic();
+		
+	}
+	
+	private void playMusic() {
 		solo.waitForActivity("cn.kuwo.player.activities.MainActivity");
 		
 		robaRandomSleep(5);
 		
 		solo.clickOnText("曲库");
 		
+		robaWaitForViewByResourceId("cn.kuwo.player:id/tv_library_new_classify");
+		
 		robaRandomClickInViews(robaGetViewsWithResourceId(TextView.class, "cn.kuwo.player:id/tv_library_new_classify"));
+		
+		Log.d(TAG, "Random choose one song repo.");
 		
 		robaRandomSleep(5);
 		
 		robaWaitForViewByResourceId("cn.kuwo.player:id/list_music_item");
 		
-		robaRandomClickInViews(robaGetViewsWithResourceId(RelativeLayout.class, "cn.kuwo.player:id/list_music_item"));
+		robaRandomDrag(DragDirection.TopButtom, 2);	
 		
-		robaRandomSleep(300);
+		robaRandomClickInViews(robaGetViewsWithResourceId(RelativeLayout.class, "cn.kuwo.player:id/list_music_item"));		
+		
+		if (isFirstStart) {
+			solo.waitForText("我知道了");
+			//robaWaitForViewByResourceId("cn.kuwo.player:id/kuwo_alert_dialog_button1");
+			solo.clickOnText("我知道了");
+		}
+		
+		robaRandomSleep(50, 100);
+		
+		Log.d(TAG, "Random choose one song.");
 		
 		solo.goBack();	
-		
 	}
 	
-	private void firstStart(String version) {
+	private void CheckFirstStart(String version) {
+		Log.d(TAG, "Check apk version for first start: " + version + ".");
 		if (version.equals("6.6.6.0")) {
-			robaWaitForViewByResourceId("cn.kuwo.player:id/guide_skipbtn");
-			robaClickOnView("cn.kuwo.player:id/guide_skipbtn");
+			if (robaWaitForViewByResourceId("cn.kuwo.player:id/guide_skipbtn") == true) {
+				robaClickOnView("cn.kuwo.player:id/guide_skipbtn");
+				isFirstStart = true;
+				solo.waitForActivity("cn.kuwo.player.activities.MainActivity");
+				
+				robaClickOnView("cn.kuwo.player:id/only_wifi_guide_delete");
+			}
 		}	
 	}
 
